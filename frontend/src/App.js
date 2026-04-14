7ca104a86e49ea15659295fa3224e365bab33ec8
Harsh vardhan <harshvardhan7112@gmail.com>
2026-04-14 13:12:23 +0530

    connect backend API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("https://imt-212-project.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMsg(data.msg))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h1>Frontend Working 🚀</h1>
+      <h2>Backend Response: {msg}</h2>
     </div>
   );
 }
